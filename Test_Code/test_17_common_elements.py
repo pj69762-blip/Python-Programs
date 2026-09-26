26490c436da5ac44f3c2e7ffe344ccb82d2cb8f8
Priyal <pj69762@gmail.com>
2026-09-26 14:51:32 +0530

    Add test_17_common_elements.py

--- Test_Code/test_17_common_elements.py
+++ Test_Code/test_17_common_elements.py
@@ -1,7 +1,7 @@
 import importlib.util
 from pathlib import Path
 
-path = Path(__file__).resolve().parents[1] / "Code" / "code_17_common_elements.py"
+path = Path(__file__).resolve().parents[1] / "Code" / "17_common_elements.py"
 spec = importlib.util.spec_from_file_location("program", path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
