53afa31a3bf383fc5a65e36aa632669fc540d568
Priyal <pj69762@gmail.com>
2026-09-26 14:49:55 +0530

    Add 06_prime_number.py

---
+++ Code/06_prime_number.py
@@ -0,0 +1,13 @@
+def is_prime(number):
+    if number < 2:
+        return False
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+
+    return True
+
+
+if __name__ == "__main__":
+    print(is_prime(7))
