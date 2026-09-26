c584654c798d820d3c22aea1cc16d9a24b3152f6
Priyal <pj69762@gmail.com>
2026-09-26 14:51:14 +0530

    Add test_10_sum_of_digits.py

--- Test_Code/test_10_sum_of_digits.py
+++ Test_Code/test_10_sum_of_digits.py
@@ -1,7 +1,7 @@
 import importlib.util
 from pathlib import Path
 
-path = Path(__file__).resolve().parents[1] / "Code" / "code_10_sum_of_digits.py"
+path = Path(__file__).resolve().parents[1] / "Code" / "10_sum_of_digits.py"
 spec = importlib.util.spec_from_file_location("program", path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
