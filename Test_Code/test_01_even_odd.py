2cb142a2b213a346aa5d0c4192c60c757c785eef
Priyal <pj69762@gmail.com>
2026-09-26 14:50:50 +0530

    Add test_01_even_odd.py

--- Test_Code/test_01_even_odd.py
+++ Test_Code/test_01_even_odd.py
@@ -1,7 +1,7 @@
 import importlib.util
 from pathlib import Path
 
-program_path = Path(__file__).resolve().parents[1] / "Code" / "code_01_even_odd.py"
+program_path = Path(__file__).resolve().parents[1] / "Code" / "01_even_odd.py"
 
 spec = importlib.util.spec_from_file_location("even_odd", program_path)
 module = importlib.util.module_from_spec(spec)
