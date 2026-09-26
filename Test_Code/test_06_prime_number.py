8d32cad474da0e975ae24a2757d7543400cf8b71
Priyal <pj69762@gmail.com>
2026-09-26 14:51:04 +0530

    Add test_06_prime_number.py

--- Test_Code/test_06_prime_number.py
+++ Test_Code/test_06_prime_number.py
@@ -1,7 +1,7 @@
 import importlib.util
 from pathlib import Path
 
-path = Path(__file__).resolve().parents[1] / "Code" / "code_06_prime_number.py"
+path = Path(__file__).resolve().parents[1] / "Code" / "06_prime_number.py"
 spec = importlib.util.spec_from_file_location("program", path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
