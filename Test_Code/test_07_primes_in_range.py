ec71e5945a02346dd21d61c1177637c0af211f33
Priyal <pj69762@gmail.com>
2026-09-26 14:51:06 +0530

    Add test_07_primes_in_range.py

--- Test_Code/test_07_primes_in_range.py
+++ Test_Code/test_07_primes_in_range.py
@@ -1,7 +1,7 @@
 import importlib.util
 from pathlib import Path
 
-path = Path(__file__).resolve().parents[1] / "Code" / "code_07_primes_in_range.py"
+path = Path(__file__).resolve().parents[1] / "Code" / "07_primes_in_range.py"
 spec = importlib.util.spec_from_file_location("program", path)
 module = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(module)
