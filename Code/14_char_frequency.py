ebf6fef0ac99821e3999a395d23d6fe2874070c5
Priyal <pj69762@gmail.com>
2026-09-26 14:50:16 +0530

    Add 14_char_frequency.py

---
+++ Code/14_char_frequency.py
@@ -0,0 +1,11 @@
+def character_frequency(text):
+    frequency = {}
+
+    for char in text:
+        frequency[char] = frequency.get(char, 0) + 1
+
+    return frequency
+
+
+if __name__ == "__main__":
+    print(character_frequency("hello"))
